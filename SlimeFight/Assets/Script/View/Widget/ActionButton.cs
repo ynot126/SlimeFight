d9e43c58489bbd74537715556d4eb7fd29452e0b
Tony <>
2026-07-09 23:15:46 +0800

    Action button add hover delay

--- SlimeFight/Assets/Script/View/Widget/ActionButton.cs
+++ SlimeFight/Assets/Script/View/Widget/ActionButton.cs
@@ -1,13 +1,31 @@
 #nullable enable
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ActionButton : CustomButton
+public class ActionButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
+    const float pointerHoverDelay = 0.2f;
+
     [SerializeField] TextMeshProUGUI actionName = null!;
     [SerializeField] Image buttonImage = null!;
+    CancellationTokenSource? pointerEnterDelayCts;
+    bool pointerEnterTriggered;
+
+    public event Action? OnPointerEnter;
+    public event Action? OnPointerExit;
+    public event Action? OnPointerClick;
+
+    void OnDestroy()
+    {
+        CancelPointerEnterDelay();
+    }
+
     public void Initialize(CharacterAction characterAction)
     {
         actionName.text = $"{characterAction.ActionName} ({characterAction.ManaCost})";
@@ -26,5 +44,48 @@ public class ActionButton : CustomButton
     public void SetHoverState(bool val)
     {
         transform.DOScale(val?1.2f:1f, 0.2f);
+    }
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        CancelPointerEnterDelay();
+        pointerEnterTriggered = false;
+        TriggerPointerEnterAfterDelay().Forget();
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        CancelPointerEnterDelay();
+        if (!pointerEnterTriggered) return;
+
+        pointerEnterTriggered = false;
+        OnPointerExit?.Invoke();
+    }
+
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+        OnPointerClick?.Invoke();
+    }
+
+    void CancelPointerEnterDelay()
+    {
+        pointerEnterDelayCts?.Cancel();
+        pointerEnterDelayCts?.Dispose();
+        pointerEnterDelayCts = null;
+    }
+
+    async UniTaskVoid TriggerPointerEnterAfterDelay()
+    {
+        pointerEnterDelayCts = new CancellationTokenSource();
+        var token = pointerEnterDelayCts.Token;
+        try
+        {
+            await UniTask.Delay((int)(pointerHoverDelay * 1000f), cancellationToken: token);
+            pointerEnterTriggered = true;
+            OnPointerEnter?.Invoke();
+        }
+        catch (OperationCanceledException)
+        {
+        }
     }
 }
